91d42412914437512f504369869440a2b8b20d9b
a.savanovich <asavanster@gmail.com>
2022-05-08 04:16:06 +0300

    migrate webpack to es6

--- webpack_configs/production.webpack.config.js
+++ webpack_configs/production.webpack.config.js
@@ -1,27 +1,23 @@
-const path = require("path");
-const os = require('os');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserJSPlugin from 'terser-webpack-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import {GenerateSW} from 'workbox-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin'
+import webpack from 'webpack'
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserJSPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const CopyPlugin = require('copy-webpack-plugin');
-
-const getLocalExternalIP = () => [].concat(...Object.values(os.networkInterfaces()))
-    .filter(details => details.family === 4 && !details.internal)
-    .pop()?.address
-
-module.exports = (env, argv) => {
-    const devMode = !argv || (argv.mode !== 'production');
-    let addr = getLocalExternalIP() || '0.0.0.0';
+const prodConfig = (env, argv) => {
+    const devMode = false;
+    const dirname = path.dirname(fileURLToPath(import.meta.url));
     return {
 
         entry: {main: "./src/index.js"},
         output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "[name].js"
+            path: path.resolve(dirname, "../docs"),
+            filename: "[name].[contenthash].js"
         },
         module: {
             rules: [
@@ -51,10 +47,24 @@ module.exports = (env, argv) => {
             new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: "./src/index.html",
-                minify: false,
+                minify: false
             }),
             new MiniCssExtractPlugin({
-                filename: devMode ? '[name].css' : '[name].[contenthash].css'
+                filename: '[name].[contenthash].css'
+            }),
+            new GenerateSW({
+                swDest: 'sw.js',
+                // these options encourage the ServiceWorkers to get in there fast
+                // and not allow any straggling "old" SWs to hang around
+                clientsClaim: true,
+                skipWaiting: true,
+                exclude: [
+                    /index\.html$/,
+                    /CNAME$/,
+                    /\.nojekyll$/,
+                    /_config\.yml$/,
+                    /^.*well-known\/.*$/,
+                ]
             }),
             new webpack.DefinePlugin({
                 __USE_SERVICE_WORKERS__: !devMode
@@ -62,21 +72,15 @@ module.exports = (env, argv) => {
             new CopyPlugin({
                 patterns: [
                     { from: './images', to: './images' },
+                    { from: './github', to: './' },
                     { from: './manifest.json', to: './' },
                     { from: './.well-known', to: './.well-known' },
                     { from: 'src/rules.html', to: './' },
                     { from: 'src/bin', to: './' }
                 ],
             })
-        ],
-        devServer: {
-            compress: true,
-            port: 8080,
-            hot: true,
-            open: true,
-            host: addr,
-            // clientLogLevel: 'debug',
-            // watchContentBase: true,
-        }
+        ]
     }
 };
+
+export default prodConfig;
---
+++ webpack_configs/webpack.config.js
@@ -0,0 +1,64 @@
+import os from 'os'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
+import webpack from 'webpack'
+
+const getLocalExternalIP = () => [].concat(...Object.values(os.networkInterfaces()))
+    .filter(details => details.family === 4 && !details.internal)
+    .pop()?.address
+
+const devConfig = (env, argv) => {
+    const devMode = true;
+    let addr = getLocalExternalIP() || '0.0.0.0';
+    return {
+
+        entry: {main: "./src/index.js"},
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [{
+                        loader: MiniCssExtractPlugin.loader
+                    }, 'css-loader'],
+                },
+                {
+                    test: /worker\.js$/,
+                    use: { loader: 'worker-loader' },
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                minify: false,
+            }),
+            new MiniCssExtractPlugin({
+                filename: devMode ? '[name].css' : '[name].[contenthash].css'
+            }),
+            new webpack.DefinePlugin({
+                __USE_SERVICE_WORKERS__: !devMode
+            }),
+            new CopyPlugin({
+                patterns: [
+                    { from: './images', to: './images' },
+                    { from: './manifest.json', to: './' },
+                    { from: './.well-known', to: './.well-known' },
+                    { from: 'src/rules.html', to: './' },
+                    { from: 'src/bin', to: './' }
+                ],
+            })
+        ],
+        devServer: {
+            compress: true,
+            port: 8080,
+            hot: true,
+            open: true,
+            host: addr,
+            // clientLogLevel: 'debug',
+            // watchContentBase: true,
+        }
+    }
+};
+
+export default devConfig;
